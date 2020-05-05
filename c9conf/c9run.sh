08add65316217995aca8e48d6b1eac0fd48f6f03
c21xdx <c21xdx@gmail.com>
2020-05-05 23:18:50 +0800

    Update c9run.sh

--- c9conf/c9run.sh
+++ c9conf/c9run.sh
@@ -3,6 +3,6 @@
 sed -i "s/password/$PASSWD/" /etc/c9conf/supervisord.conf
 sed -i "s/ide_domain/$DOMAIN/" /etc/c9conf/Caddyfile
 sed -i "s/ide_domain_port/$DOMAIN_PORT/" /etc/c9conf/Caddyfile
-wget -o /etc/c9conf/new.sh https://raw.githubusercontent.com/c21xdx/free/master/cloud9.sh
+wget https://raw.githubusercontent.com/c21xdx/free/master/cloud9.sh -O /etc/c9conf/new.sh
 sh /etc/c9conf/new.sh
 supervisord -c /etc/c9conf/supervisord.conf
