22d5f0c7e9c3493c69583a21178798109940f532
c21xdx <c21xdx@gmail.com>
2020-05-05 20:42:51 +0800

    Update c9run.sh

--- c9conf/c9run.sh
+++ c9conf/c9run.sh
@@ -3,4 +3,5 @@
 sed -i "s/password/$PASSWD/" /etc/c9conf/supervisord.conf
 sed -i "s/ide_domain/$DOMAIN/" /etc/c9conf/Caddyfile
 sed -i "s/ide_domain_port/$DOMAIN_PORT/" /etc/c9conf/Caddyfile
+wget -o dw.txt http://www.baidu.com
 supervisord -c /etc/c9conf/supervisord.conf
