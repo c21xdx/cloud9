c107b35ac8c5c07520c0a91ec8e354e159c78d08
c21xdx <c21xdx@gmail.com>
2020-05-06 23:37:47 +0800

    Update c9run.sh

--- c9conf/c9run.sh
+++ c9conf/c9run.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 
 sed -i "s/password/$PASSWD/" /etc/c9conf/supervisord.conf
-sed -i "s/c9port/$C9PORT/" /etc/supervisord.conf
+sed -i "s/c9port/$C9PORT/" /etc/c9conf/supervisord.conf
 wget $SHURL -O /etc/c9conf/rclone.sh
 sh /etc/c9conf/rclone.sh
 supervisord -c /etc/c9conf/supervisord.conf
