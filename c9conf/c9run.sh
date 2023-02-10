99f60dbc3a08e5817c44a16f9c92f25c78b8ab91
c21xdx <c21xdx@gmail.com>
2023-02-10 18:53:33 +0800

    Update c9run.sh

--- c9conf/c9run.sh
+++ c9conf/c9run.sh
@@ -5,6 +5,9 @@ export PATH=$PATH:/usr/local/go/bin
 NODE_ENV="production"
 export PATH="${PATH}:/root/.c9/node/bin"
 
+export FLYCTL_INSTALL="/root/.fly"
+export PATH="$FLYCTL_INSTALL/bin:$PATH"
+
 # 密码及端口修改
 sed -i "s/password/$PASSWD/" /etc/c9conf/supervisord.conf
 sed -i "s/c9port/$C9PORT/" /etc/c9conf/supervisord.conf
@@ -17,7 +20,7 @@ chmod 600 /var/spool/cron/crontabs/root
 service cron restart
 
 # 其它的...
-wget $SHURL -O /etc/c9conf/others.sh
-sh /etc/c9conf/others.sh
+# wget $SHURL -O /etc/c9conf/others.sh
+# sh /etc/c9conf/others.sh
 
 supervisord -c /etc/c9conf/supervisord.conf
