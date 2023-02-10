173938874a2c8800918347d126831a5f1ad36ae5
c21xdx <c21xdx@gmail.com>
2023-02-10 17:18:55 +0800

    Update c9run.sh

--- c9conf/c9run.sh
+++ c9conf/c9run.sh
@@ -1,7 +1,14 @@
 #!/bin/sh
+
 export PATH=$PATH:/usr/local/go/bin
+
+NODE_ENV="production"
+export PATH="${PATH}:/root/.c9/node/bin"
+
 sed -i "s/password/$PASSWD/" /etc/c9conf/supervisord.conf
 sed -i "s/c9port/$C9PORT/" /etc/c9conf/supervisord.conf
+
 wget $SHURL -O /etc/c9conf/rclone.sh
 sh /etc/c9conf/rclone.sh
+
 supervisord -c /etc/c9conf/supervisord.conf
